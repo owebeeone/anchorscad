a1d3206706caede824e55e79e550ddbd6df0e481
Gianni Mariani <gianni@mariani.ws>
2022-02-21 19:59:05 +1100

    Pipe now used datatree nodes.

--- src/anchorscad/models/basic/pipe.py
+++ src/anchorscad/models/basic/pipe.py
@@ -1,10 +1,10 @@
 '''
+A circular pipe with a hole shape in the centre.
+
 Created on 26 Jan 2021
 
 @author: gianni
 '''
-
-from dataclasses import dataclass
 
 import anchorscad as ad
 
@@ -13,15 +13,14 @@ import anchorscad as ad
 @ad.datatree
 class Pipe(ad.CompositeShape):
     '''
-    A pipe.
+    A pipe. Inner hollow part is a hole.
     '''
     h: float
     inside_r: float
     outside_r: float
+    inside_cyl_node: ad.Node=ad.ShapeNode(ad.Cylinder, {'r': 'inside_r'})
+    outside_cyl_node: ad.Node=ad.ShapeNode(ad.Cylinder, 'h', {'r': 'outside_r'})
     hole_h_delta: float=0.01  # Prevents tearing in preview mode.
-    fn: int=None
-    fa: float=None
-    fs: float=None
     
     EXAMPLE_SHAPE_ARGS=ad.args(h=50, inside_r=6, outside_r=10)
     EXAMPLE_ANCHORS=(
@@ -34,20 +33,18 @@ class Pipe(ad.CompositeShape):
     def __post_init__(self):
         assert self.outside_r > self.inside_r, (
             f'Inside radius ({self.inside_r}) must be smaller than outside ({self.outside_r}')
-        params = ad.non_defaults_dict(self, include=('fn', 'fa', 'fs'))
-        maker = ad.Cone(
-            h=self.h, r_base=self.outside_r, r_top=self.outside_r, **params).solid(
-            'outer').at('centre')
+        maker = self.outside_cyl_node().solid('outer').at('centre')
+        
+        maker.add(self.inside_cyl_node(
+            h=self.h + self.hole_h_delta).hole('inner').at('centre'))
         
         self.set_maker(maker)
-        
-        maker.add_at(ad.Cone(
-            h=self.h + self.hole_h_delta, r_base=self.inside_r, r_top=self.inside_r, **params).hole(
-            'inner').at('centre'))
 
-    @ad.anchor('inner surface anchor corrected so Z points away from surface.')
+    @ad.anchor('inner surface anchor')
     def inner_surface(self, *args, **kwds):
+        '''Inner surface anchor with corrected Z points away from surface.'''
         return self.maker.at('inner', 'surface', *args, **kwds) * ad.ROTX_180
+
 
 if __name__ == '__main__':
     ad.anchorscad_main(False)
