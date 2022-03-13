c7d7aa1c1a05c79046686578fa81a6c9e024b674
Gianni Mariani <gianni@mariani.ws>
2022-03-13 15:59:22 +1100

    Add SquarePipe documentation.

--- src/anchorscad/models/basic/SquarePipe.py
+++ src/anchorscad/models/basic/SquarePipe.py
@@ -10,16 +10,16 @@ EPSILON=1.0e-3
 @ad.shape
 @ad.datatree
 class SquarePipe(ad.CompositeShape):
-    '''Pipe with box section consisting of an outer box with an inner box hole.
-    '''
-    size: tuple
-    wall_size: float=5.0
+    '''Pipe with box section consisting of an outer box with an 
+    inner box hole.'''
+    size: tuple=ad.dtfield(doc='Overall size of SquarePipe shape.')
+    wall_size: float=ad.dtfield(5.0, 'Wall thickness of SquarePipe shape')
     
     EXAMPLE_SHAPE_ARGS=ad.args((70, 50, 30))
     EXAMPLE_ANCHORS=(ad.surface_args('face_centre', 5),
                      ad.surface_args('inner', 'face_centre', 2),)
     
-    def __post_init__(self):
+    def build(self) -> ad.Maker:
         maker = ad.Box(self.size).solid('outer').at('centre')
         # Make the inner box slightly larger to stop tearing when rendered.
         inner_size = (self.size[0] - 2 * self.wall_size,
@@ -27,7 +27,7 @@ class SquarePipe(ad.CompositeShape):
                       self.size[2] + EPSILON)
         maker2 = ad.Box(inner_size).hole('hole').at('centre')
         maker.add_at(maker2, 'centre')
-        self.set_maker(maker)
+        return maker
 
     @ad.anchor('Inner hole.')
     def inner(self, *args, **kwds):
@@ -38,14 +38,13 @@ class SquarePipe(ad.CompositeShape):
 @ad.shape
 @ad.datatree
 class SquarePipeTee(ad.CompositeShape):
-    '''A tee of SquarePipe.
-    '''
-    size: tuple
-    wall_size: float=5.0
-
+    '''A tee of 3 SquarePipe shapes all the same size.'''
+    size: tuple=ad.dtfield(doc='Overall size of SquarePipe shape.')
+    wall_size: float=ad.dtfield(5.0, 'Wall thickness of SquarePipe shape')
+    
     EXAMPLE_SHAPE_ARGS=ad.args((50, 50, 35))
     
-    def __post_init__(self):
+    def build(self) -> ad.Maker:
         shape = SquarePipe(self.size, self.wall_size)
 
         maker = shape.composite('LHS').at('face_centre', 1)
@@ -54,8 +53,9 @@ class SquarePipeTee(ad.CompositeShape):
         maker.add_at(shape.composite('stem').at('face_centre', 1),
                      'face_centre', 1, post=ad.ROTX_90)
         
-        self.set_maker(maker)
+        return maker
 
 
+MAIN_DEFAULT=ad.ModuleDefault(True) # Set default for --write
 if __name__ == '__main__':
     ad.anchorscad_main()
