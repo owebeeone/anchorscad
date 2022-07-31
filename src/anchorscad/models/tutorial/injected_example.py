0f7f25b1a4ca29726894940d3406e10dac20e7f5
Gianni Mariani <owebeeone@gmail.com>
2022-07-31 19:38:15 +1000

    Added clock tutorial example.

---
+++ src/anchorscad/models/tutorial/injected_example.py
@@ -0,0 +1,33 @@
+'''
+Created on 31 July 2022
+
+@author: gianni
+'''
+
+import anchorscad as ad
+
+
+@ad.shape
+@ad.datatree
+class ClockFace(ad.CompositeShape):
+    '''Simple clock face with different sized spheres at the points.'''
+    count_points: int=ad.dtfield(12, 'The number of points on the clock face')
+    r_mod_map: tuple=ad.dtfield(((12, 6), (3, 4), (1, 3)), 'Radius at each point')
+    x_pos: float=ad.dtfield(40, 'Radius of the clock face centres')
+    sphere_node: ad.Node=ad.dtfield(ad.ShapeNode(ad.Sphere), init=False)
+
+    def build(self) -> ad.Maker:
+        maker = ad.Cylinder(r=self.x_pos).cage('cage').at('centre')
+        for i in range(self.count_points):
+            for div, r in self.r_mod_map:
+                if i % div == 0:
+                    maker.add_at(self.sphere_node(r=r + self.r)
+                                 .solid(('point', i)).at('centre'),
+                        'surface', degrees=i * 360 / self.count_points)
+                    break
+        return maker
+
+MAIN_DEFAULT=ad.ModuleDefault(True)
+
+if __name__ == "__main__":
+    ad.anchorscad_main()
