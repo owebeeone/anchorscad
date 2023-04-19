f448e3817501da4848011d11c0c4c2f0b110905c
Gianni Mariani <owebeeone@gmail.com>
2023-04-19 23:31:26 +1000

    Updated tactile terminal block models to use the latest api.

--- src/anchorscad/models/components/terminal_blocks/terminal_kf301.py
+++ src/anchorscad/models/components/terminal_blocks/terminal_kf301.py
@@ -4,17 +4,14 @@ Created on 12 Jan 2022
 @author: gianni
 '''
 
-from dataclasses import dataclass
-import anchorscad.core as core
-import anchorscad.extrude as e 
-from anchorscad.datatrees import datatree, Node
-import anchorscad.linear as l
+
+import anchorscad as ad
 
 EPSILON=0.001
 
-@core.shape
-@datatree
-class TerminalKF301Terminal(core.CompositeShape):
+@ad.shape
+@ad.datatree
+class TerminalKF301Terminal(ad.CompositeShape):
     
     h: float=10
     w: float=7.6
@@ -29,17 +26,17 @@ class TerminalKF301Terminal(core.CompositeShape):
     term_hole_r: float=2.9 / 2
     term_hole_y: float=0.5
     term_hole_fn: int=32
-    term_hole_node: Node=core.Node(core.Cylinder, 
+    term_hole_node: ad.Node=ad.Node(ad.Cylinder, 
                                    {'r': 'term_hole_r', 'fn': 'term_hole_fn'})
     
     pin_fn: int=8
-    pin_node: Node=core.Node(core.Cylinder, 
+    pin_node: ad.Node=ad.Node(ad.Cylinder, 
                                    {'r': 'pin_r', 'fn': 'pin_fn'})
     
     screw_fn: int=32
     screw_r: float=3.35 / 2
     screw_h: float=10
-    screw_node: Node=core.Node(core.Cylinder, 
+    screw_node: ad.Node=ad.Node(ad.Cylinder, 
                                    {'r': 'screw_r', 
                                     'h': 'screw_h', 
                                     'fn': 'screw_fn'})
@@ -50,26 +47,26 @@ class TerminalKF301Terminal(core.CompositeShape):
     
     pin_xyz_offs: tuple=(0, block_x_offs, -0.8)
     
-    cage_node: Node=core.CageOfNode()
+    cage_node: ad.Node=ad.CageOfNode()
     
-    EXAMPLE_SHAPE_ARGS=core.args(as_cage=False)
-    EXAMPLE_ANCHORS=(core.surface_args('face_edge', 1, 0, scale_anchor=0.3),
-                     core.surface_args('pin', 'top', scale_anchor=0.3),)
+    EXAMPLE_SHAPE_ARGS=ad.args(as_cage=False)
+    EXAMPLE_ANCHORS=(ad.surface_args('face_edge', 1, 0, scale_anchor=0.3),
+                     ad.surface_args('pin', 'top', scale_anchor=0.3),)
     
     def __post_init__(self):
         size = (self.w, self.pin_pitch, self.h)
-        maker = self.cage_node(core.Box(size)).at('face_edge', 1, 0, post=l.ROTY_180)
+        maker = self.cage_node(ad.Box(size)).at('face_edge', 1, 0, post=ad.ROTY_180)
         
-        block_shape = core.Box(self.term_size)
+        block_shape = ad.Box(self.term_size)
         
         maker.add_at(block_shape.solid('block').at('face_edge', 1, 3),
-                     'face_edge', 1, 3, post=l.translate(self.pin_xyz_offs))
+                     'face_edge', 1, 3, post=ad.translate(self.pin_xyz_offs))
         
         term_hole = self.term_hole_node(h=self.term_size[0] + 2 * EPSILON)
         
         maker.add_at(term_hole.hole('term_hole').at('surface'),
                      'block', 'face_edge', 1, 3, 
-                     post=l.translate([0, -EPSILON, -0.5]))
+                     post=ad.translate([0, -EPSILON, -0.5]))
         
         pin_h = self.pin_h - self.pin_xyz_offs[2]
         pin_shape = self.pin_node(h=pin_h)
@@ -84,17 +81,17 @@ class TerminalKF301Terminal(core.CompositeShape):
                      .at('base', rh=1),
                      'block', 'face_centre', 4)
         
-        block_access_shape = core.Box(self.block_access_size)
+        block_access_shape = ad.Box(self.block_access_size)
         maker.add_at(block_access_shape
                      .solid_hole('block_access', self.block_access_as_hole)
                      .at('face_centre', 2),
-                     'block', 'face_centre', 2, post=l.ROTY_180)
+                     'block', 'face_centre', 2, post=ad.ROTY_180)
         
         self.set_maker(maker)
 
-@core.shape
-@datatree
-class TerminalKF301Body(core.CompositeShape):
+@ad.shape
+@ad.datatree
+class TerminalKF301Body(ad.CompositeShape):
     '''
     Based on the specs for Handson Technologu KF301.
     '''
@@ -107,20 +104,20 @@ class TerminalKF301Body(core.CompositeShape):
     trap_p2: float=1.0
     
     count: int=3
-    cage_node: Node=core.CageOfNode()
+    cage_node: ad.Node=ad.CageOfNode()
     
-    EXAMPLE_SHAPE_ARGS=core.args(as_cage=True)
-    EXAMPLE_ANCHORS=(core.surface_args('face_edge', 1, 0),)
+    EXAMPLE_SHAPE_ARGS=ad.args(as_cage=True)
+    EXAMPLE_ANCHORS=(ad.surface_args('face_edge', 1, 0),)
     
     def __post_init__(self):
         
         length = self.count * self.pin_pitch
-        cage_shape = core.Box((self.w, length, self.h))
+        cage_shape = ad.Box((self.w, length, self.h))
         
-        maker = self.cage_node(cage_shape).at('face_edge', 1, 0, post=l.ROTY_180)
+        maker = self.cage_node(cage_shape).at('face_edge', 1, 0, post=ad.ROTY_180)
         
         trap_y = self.h - self.trap_p0
-        path = (e.PathBuilder()
+        path = (ad.PathBuilder()
                 .move((0, 0), direction=(-1, 0))
                 .stroke(self.w / 2, name='base_lhs')
                 .stroke(self.trap_p0, sinr_cosr=(-1, 0), name='lower_lhs')
@@ -132,46 +129,44 @@ class TerminalKF301Body(core.CompositeShape):
                 .line((0, 0), 'base_rhs')
                 .build())
         
-        shape = e.LinearExtrude(path, length)
+        shape = ad.LinearExtrude(path, length)
         maker.add_at(shape.solid('terminal').at('base_lhs', rh=1),
-                     'face_edge', 1, 0, post=l.ROTY_180 * l.ROTZ_180)
+                     'face_edge', 1, 0, post=ad.ROTY_180 * ad.ROTZ_180)
         
 
         self.set_maker(maker)
 
-    @core.anchor('An example anchor specifier.')
+    @ad.anchor('An example anchor specifier.')
     def side(self, *args, **kwds):
         return self.maker.at('face_edge', *args, **kwds)
     
 
-@core.shape
-@datatree
-class TerminalKF301(core.CompositeShape):
+@ad.shape
+@ad.datatree
+class TerminalKF301(ad.CompositeShape):
     '''
     Based on the specs for Handson Technologu KF301.
     '''
     count: int=2
     
-    body_node: Node=core.ShapeNode(TerminalKF301Body)
+    body_node: ad.Node=ad.ShapeNode(TerminalKF301Body)
     
-    terminal: Node=core.ShapeNode(TerminalKF301Terminal)
+    terminal: ad.Node=ad.ShapeNode(TerminalKF301Terminal)
     
-    EXAMPLE_SHAPE_ARGS=core.args(as_cage=True, 
+    EXAMPLE_SHAPE_ARGS=ad.args(as_cage=True, 
                                  block_access_as_hole=True,
                                  screw_access_as_hole=True)
-    EXAMPLE_ANCHORS=(core.surface_args('face_edge', 1, 0),)
+    EXAMPLE_ANCHORS=(ad.surface_args('face_edge', 1, 0),)
     
-    def __post_init__(self):
-        
+    def build(self) -> ad.Maker:
         maker = self.body_node().solid('body').at()
         
         terminal_shape = self.terminal()
         for i in range(self.count):
             maker.add_at(terminal_shape.composite(('terminal', i + 1))
                          .at('face_edge', 3, 0), 
-                         'face_edge', 3, 0, post=l.tranZ(-i * self.pin_pitch))
-            
-        self.set_maker(maker)
+                         'face_edge', 3, 0, post=ad.tranZ(-i * self.pin_pitch))
+        return maker
         
 if __name__ == '__main__':
-    core.anchorscad_main(False)
+    ad.anchorscad_main(False)
