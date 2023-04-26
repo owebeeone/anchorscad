b3224f447affce5b817c195da5987760e9c45d16
Gianni Mariani <owebeeone@gmail.com>
2023-04-26 23:27:24 +1000

    TriangularPrism now uses latest anchorscad package only API.

--- src/anchorscad/models/basic/TriangularPrism.py
+++ src/anchorscad/models/basic/TriangularPrism.py
@@ -4,52 +4,49 @@ Created on 12 Sep 2021
 @author: gianni
 '''
 
-from dataclasses import dataclass
-import anchorscad.core as core
-from anchorscad import linear as l
-from anchorscad.extrude import PathBuilder, LinearExtrude
+import anchorscad as ad
 
 
-@core.shape
-@dataclass
-class TriangularPrism(core.CompositeShape):
+@ad.shape
+@ad.datatree
+class TriangularPrism(ad.CompositeShape):
     '''
     Generates a triangular prism given the width depth and height.
-    This is caged by a core.Box for box anchors and also named 2D path
+    This is caged by a ad.Box for box anchors and also named 2D path
     anchors.
     '''
     size: tuple
     
-    EXAMPLE_SHAPE_ARGS=core.args([30, 10, 3])
-    EXAMPLE_ANCHORS=(core.surface_args('base'),
-                     core.surface_args('face_centre', 1),)
+    EXAMPLE_SHAPE_ARGS=ad.args([30, 10, 3])
+    EXAMPLE_ANCHORS=(ad.surface_args('base'),
+                     ad.surface_args('face_centre', 1),)
     
-    def __post_init__(self):
-        maker = core.Box(self.size).cage(
+    def build(self) -> ad.Maker:
+        maker = ad.Box(self.size).cage(
             'cage').at('centre')
         
-        path = (PathBuilder()
+        path = (ad.PathBuilder()
             .move([0, 0])
             .line([0, self.size[0]], 'face1')
             .line([self.size[1], 0], 'face2')
             .line([0, 0], 'face3')
             .build())
             
-        shape = LinearExtrude(path, h=self.size[2])
+        shape = ad.LinearExtrude(path, h=self.size[2])
         
         maker.add_at(shape.solid('prism').at('face3', 0.5, rh=0.5), 
-                     'face_centre', 2, post=l.ROTX_180)
+                     'face_centre', 2, post=ad.ROTX_180)
         
-        self.set_maker(maker)
+        return maker
 
-    @core.anchor('Base of the prism.')
+    @ad.anchor('Base of the prism.')
     def base(self, *args, **kwds):
         return self.maker.at('prism', 'face3', 1, *args, **kwds)
     
     
 # Uncomment the line below to default to writing OpenSCAD files
 # when anchorscad_main is run with no --write or --no-write options.
-MAIN_DEFAULT=core.ModuleDefault(True)
+MAIN_DEFAULT=ad.ModuleDefault(True)
 
 if __name__ == '__main__':
-    core.anchorscad_main(False)
+    ad.anchorscad_main(False)
