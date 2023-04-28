d239428423a7a2c7c4a7a0b6edaa350d0de450b4
Gianni Mariani <owebeeone@gmail.com>
2023-04-29 00:38:18 +1000

    Move Tnut models to new api

--- src/anchorscad/models/screws/tnut.py
+++ src/anchorscad/models/screws/tnut.py
@@ -26,38 +26,37 @@ class Tnut(ad.CompositeShape):
     fn: int=32
     fa: float=None
     fs: float=None
+    cyliner_node: ad.Node=ad.ShapeNode(ad.Cylinder, {})
+    cone_node: ad.Node=ad.ShapeNode(ad.Cone, {})
     
     EXAMPLE_SHAPE_ARGS=ad.args()
     
-    
-    def __post_init__(self):
-        kwds = {'fn': self.fn, 'fs': self.fs, 'fa': self.fa}
-        shaft_cage_shape = ad.Cylinder(
-            h=self.h_shaft, r=self.r_shaft, **kwds)
+    def build(self) -> ad.Maker:
+        shaft_cage_shape = self.cyliner_node(
+            h=self.h_shaft, r=self.r_shaft)
         
         maker = shaft_cage_shape.cage('cage').at('base')
         
-        base_shape = ad.Cylinder(h=self.h_t, r=self.r_t, **kwds)
+        base_shape = self.cyliner_node(h=self.h_t, r=self.r_t)
         
         maker.add_at(base_shape.solid('flat').at('base'),
                      'base')
-        bevel_shape = ad.Cone(h=self.bevel_shaft + epsilon2, 
+        bevel_shape = self.cone_node(h=self.bevel_shaft + epsilon2, 
                                 r_base=self.r_shaft + self.bevel_shaft, 
-                                r_top=self.r_shaft, 
-                                **kwds)
+                                r_top=self.r_shaft)
         maker.add_at(bevel_shape.solid('bevel').at('base'),
                      'flat', 'base', rh=1, h=-epsilon)
         
         h_shaft = self.h_shaft - self.bevel_shaft - self.h_t
         
-        shaft_cage_shape = ad.Cylinder(
-            h=h_shaft, r=self.r_shaft, **kwds)
+        shaft_cage_shape = self.cyliner_node(
+            h=h_shaft, r=self.r_shaft)
         
         maker.add_at(shaft_cage_shape.solid('shaft').at('base'),
                      'bevel', 'base', rh=1, h=-epsilon)
         
-        shaft_extension = ad.Cylinder(
-            h=h_shaft, r=self.r_shaft, **kwds)
+        shaft_extension = self.cyliner_node(
+            h=h_shaft, r=self.r_shaft)
         
         maker.add_at(shaft_extension.solid('shaft_extension')
                      .colour((1, 0, 0))
@@ -77,7 +76,7 @@ class Tnut(ad.CompositeShape):
                          'flat', 'surface', degrees=i * 360 / 4
                          )
         
-        self.set_maker(maker)
+        return maker
         
 
     @ad.anchor('An example anchor')
@@ -88,27 +87,27 @@ class Tnut(ad.CompositeShape):
 @ad.shape
 @ad.datatree
 class TnutExample(ad.CompositeShape):
-    tnut_type: ad.Shape=Tnut
     offs: float=4
     fn: int=128
     fa: float=None
     fs: float=None
+    tnut_node: ad.Node=ad.ShapeNode(Tnut, {})
+    bsb_node: ad.Node=ad.ShapeNode(bsb.BoxSideBevels, {})
     
-    def __post_init__(self):
-        kwds = {'fn': self.fn, 'fs': self.fs, 'fa': self.fa}
-        tnut = self.tnut_type(**kwds)
+    def build(self) -> ad.Maker:
+        tnut = self.tnut_node()
         
         sizexy = tnut.r_t * 2 + self.offs
 
-        maker = bsb.BoxSideBevels(
-            (sizexy, sizexy, tnut.h_shaft), self.offs, **kwds)\
+        maker = self.bsb_node(
+            (sizexy, sizexy, tnut.h_shaft), self.offs)\
                 .solid('model').at('centre')
                 
         maker.add_at(tnut.hole('tnut').at('base'),
                      'face_centre', 4,
                      post=ad.tranZ(epsilon))
+        return maker
 
-        self.set_maker(maker)
 
 if __name__ == "__main__":
     ad.anchorscad_main(False)
