09016446098c4258009cea39c1e5920fbe8949c7
Gianni Mariani <owebeeone@gmail.com>
2024-06-11 01:36:36 +1000

    Adding Adrian's old lens cap model

---
+++ src/anchorscad/models/lens_cap/lens_cap.py
@@ -0,0 +1,273 @@
+'''
+Created on 27 Jan 2022
+
+@author: adrian
+'''
+
+from numpy import pi
+
+import anchorscad as ad
+from anchorscad import datatree, Node, PathBuilder, Path, RotateExtrude
+
+import numpy as np
+
+
+PRINT_ERROR_OFFSET = .25
+
+COVER_CAVITY_R = 50.5 / 2 + PRINT_ERROR_OFFSET
+OUTER_CYL_R1 = 60 / 2
+OUTER_CYL_R2 = 56 / 2
+
+TOTAL_H = 19.1
+FLANGE_H = 4.65
+COVER_H = TOTAL_H - FLANGE_H
+
+COVER_DEPTH = 11.96
+TOTAL_DEPTH = COVER_DEPTH + FLANGE_H
+
+FLANGE_THICKNESS = FLANGE_H - 1.94
+FLANGE_CAVITY_R = 55.1 / 2 + PRINT_ERROR_OFFSET
+
+
+@datatree
+class SharedDimensions:
+    inner_h: float=COVER_DEPTH
+    inner_r: float=COVER_CAVITY_R
+    
+    flange_h: float=FLANGE_H
+    flange_r: float=FLANGE_CAVITY_R
+    
+    stock_r_top: float=OUTER_CYL_R1
+    stock_r_base: float=OUTER_CYL_R2
+    
+    epsilon: float=.003
+    
+
+@ad.shape
+@datatree
+class BasicCavityOutline(ad.CompositeShape, SharedDimensions):
+    inner_cavity_node: Node=ad.ShapeNode(ad.Cylinder, prefix='inner_')
+    flange_cavity_node: Node=ad.ShapeNode(ad.Cylinder, prefix='flange_')
+    
+    fn: int=128
+    
+    def __post_init__(self):
+        maker = self.inner_cavity_node().solid('inner_cavity').at('centre')
+        
+        maker.add_at(self.flange_cavity_node().solid('flange_cavity').
+                     at('base'), 
+                     'base', rh=1, post=ad.tranZ(self.epsilon))
+        
+        self.maker = maker
+        
+        
+@ad.shape
+@datatree
+class TaperedFlange(ad.CompositeShape, SharedDimensions):
+    
+    flange_shape_h: float=FLANGE_THICKNESS
+    flange_shape_r: float=OUTER_CYL_R2
+    flange_shape_inner_r: float=OUTER_CYL_R2 - (3.98 - 2.3)
+    flange_shape_node: Node=ad.ShapeNode(ad.Cylinder, prefix='flange_shape_')
+    
+    flange_extrude_path: Path=None
+    flange_extrude_degrees: float=55
+    flange_extrude_node: Node=ad.ShapeNode(RotateExtrude, prefix='flange_extrude_')
+    
+    fn: float=128
+   
+    def __post_init__(self):
+        path = (PathBuilder()
+                .move([self.flange_shape_inner_r, 0], name='origin')
+                .line([self.flange_shape_inner_r, self.flange_shape_h], name='inner')
+                .line([self.flange_shape_r, self.flange_shape_h], name='top')
+                .line([self.flange_shape_r, 0], name='outer')
+                .line([self.flange_shape_inner_r, 0], name='base')
+                .build())
+        
+        extrusion = self.flange_extrude_node(path)
+        
+        flange = extrusion.solid('extrusion')\
+                    .at('base', degrees=self.flange_extrude_degrees / 2)
+                    
+        intersector_size = [self.flange_r,
+                            self.flange_r,
+                            self.flange_shape_h * 2]
+        intersector = ad.Box(intersector_size)
+        
+        raised_taper = .3
+        taper_angle = 181.5
+        flange.add_at(intersector.solid('intersector')
+                      .at('face_centre', 1, 
+                          post=ad.rotY(taper_angle) * ad.tranZ(-raised_taper)), 
+                      post=ad.IDENTITY)
+        
+        extents = path.extents()
+        
+        self.maker = flange.intersect('tapered_flange').at()
+        
+        size_yz = extents[1] - extents[0]
+        size_stopper = (2, size_yz[0], size_yz[1] + 1)
+        
+        stopper = ad.Box(size_stopper).solid('stopper')
+        
+        self.maker.add_at(stopper.at('face_corner', 0, 0, 
+                                     post=ad.ROTZ_180 * ad.ROTX_90), 
+                          'top', 1)
+        
+
+@ad.shape
+@datatree
+class FlangeOutline(ad.CompositeShape, SharedDimensions):
+#     cavity_node: Node=ad.ShapeNode(BasicCavityOutline)
+    
+    taper_node: Node=ad.ShapeNode(TaperedFlange)
+
+    taper_shape: ad.Shape=ad.dtfield(self_default=lambda s: s.taper_node())
+    
+    flange_cage_shape: ad.Shape=ad.dtfield(self_default=lambda s: s.taper_shape.flange_shape_node())
+    
+    flange_cage_node: Node=ad.Node(ad.cageof, prefix='flange_cage_')
+    
+    EXAMPLE_SHAPE_ARGS=ad.args(flange_cage_hide_cage=False)
+    
+    def build(self) -> ad.Maker:
+        
+        self.flange_cage_shape = self.flange_cage_shape
+        
+        flange_cage = self.flange_cage_node()
+        
+        maker = flange_cage.at()
+        
+        extrusion = self.taper_node()
+        
+        for i in range(3):
+            maker.add_at(extrusion.solid(('flange', i))
+                              .at('outer', 1, post=ad.ROTX_180),
+                              'surface', 0, degrees=(360 / 3) * i)
+            
+        return maker
+
+
+# @ad.shape
+# @datatree
+# class GripOutline(ad.CompositeShape, SharedDimensions):
+#     diameter: float=2 * SharedDimensions.stock_r_top
+#     circumference: float=diameter * np.pi
+    
+#     grip_width: float=2
+#     grip_count: int=360 / grip_width
+#     grip_angle: float=circumference / grip_count
+       
+        
+@ad.shape
+@datatree
+class ShellAssembly(ad.CompositeShape):
+    cavity_node: Node=ad.ShapeNode(BasicCavityOutline)
+    
+    flange_outline_node: Node=ad.ShapeNode(FlangeOutline) 
+    
+    stock_h: float=TOTAL_H
+    stock_cyl_node: Node=ad.ShapeNode(ad.Cone, prefix='stock_')   
+    
+    EXAMPLE_SHAPE_ARGS=ad.args(flange_cage_hide_cage=True)     
+    
+    def build(self) -> ad.Maker:
+        outline = self.stock_cyl_node().solid('stock').at()
+        
+        outline.add_at(self.cavity_node().hole('cavity')
+                     .at('flange_cavity', 'top', post=ad.tranZ(-self.epsilon)), 
+                     'top')        
+        
+        maker = outline.solid('outline').at()
+        
+        flanges = self.flange_outline_node()
+        
+        maker.add_at(flanges.solid('flanges').at('top'), 'top')
+        
+        return maker
+
+
+@datatree
+class GripRibPath:
+
+    grip_h: float=13
+    grip_d: float=1.3
+
+    def build(self) -> ad.Path:
+        builder = (ad.PathBuilder()
+            .move((0, 0))
+            .line((self.grip_h, 0), 'base')
+            .arc_points(
+                middle=(self.grip_h / 2, self.grip_d),
+                last=(0, 0),
+                name='top'))
+        
+        return builder.build()
+
+
+@ad.shape
+@datatree
+class GripRib(ad.CompositeShape):
+
+    path_node: Node=ad.ShapeNode(GripRibPath)
+    grip_path: ad.Path=ad.dtfield(self_default=lambda s: s.path_node().build())
+    grip_w: float=3.0
+
+    extrude_node: Node=ad.ShapeNode(ad.LinearExtrude, {'h': 'grip_w', 'path': 'grip_path'})
+
+    def build(self) -> ad.Maker:
+        
+        return self.extrude_node().solid('grip_rib').at()
+        
+@ad.shape
+@datatree
+class LensCap(ad.CompositeShape):
+    assembly_node: Node=ad.ShapeNode(ShellAssembly)
+
+    grip_rib_node: Node=ad.ShapeNode(GripRib)
+    grip_rib_count: int=32
+    grip_rib_zoffs: float=0.3
+    grip_rib_as_recess: bool=True
+
+    EXAMPLE_SHAPE_ARGS=ad.args(fn=256, grip_rib_as_recess=False)
+
+    EXAMPLES_EXTENDED={
+        'recessed_rib': ad.ExampleParams(
+            shape_args=ad.args(fn=256, grip_rib_as_recess=True),
+            anchors=()
+            )
+        }
+
+    def build(self) -> ad.Maker:
+        maker = self.assembly_node().composite('assembly').at()
+
+        for i in range(self.grip_rib_count):
+
+            degrees = (360 / self.grip_rib_count) * i
+            
+            if self.grip_rib_as_recess:
+                orientation = ad.IDENTITY
+                zoffs = self.grip_rib_zoffs
+                msf = ad.ModeShapeFrame.HOLE
+            else:
+                orientation = ad.ROTX_180
+                zoffs = -self.grip_rib_zoffs
+                msf = ad.ModeShapeFrame.SOLID 
+
+            rib_maker = self.grip_rib_node().named_shape(('grip_rib', i), msf) \
+                .at('base', 0.5, rh=0.5, post=ad.ROTZ_90 * orientation)
+            maker.add_at(
+                rib_maker,
+                'outline', 'stock', 'surface', degrees=degrees, rh=0.5, 
+                post=ad.tranZ(zoffs))
+
+        return maker
+    
+
+# Uncomment the line below to default to writing OpenSCAD files
+# when anchorscad_main is run with no --write or --no-write options.
+MAIN_DEFAULT=ad.ModuleDefault(all=True)
+
+if __name__ == '__main__':
+    ad.anchorscad_main(False)
---
+++ src/anchorscad/models/lens_cap/sensor_cap.py
@@ -0,0 +1,196 @@
+'''
+Created on 27 Jan 2022
+
+@author: adrian
+'''
+
+from typing import Tuple
+import anchorscad as ad
+from anchorscad import datatree, Node, PathBuilder, LinearExtrude, Path
+import numpy as np
+
+
+def to_2d(gvec) -> np.ndarray:
+    return gvec.A[0:2]
+
+INNER_CYL_R = 41.6 / 2
+OUTER_CYL_R = 50 / 2
+
+INNER_CYL_THICKNESS = 4.15
+OUTER_CYL_THICKNESS = 1.95
+
+TAB_WITDH = 20.2 - 4
+TAB_DEPTH = 2.65 - .25
+
+KEY_HEIGHT = 2.5
+KEY_THICKNESS = 2.5
+KEY_OFFSET = 44.68 - INNER_CYL_R * 2
+
+    
+@ad.shape
+@datatree
+class LockTabsAssembly(ad.CompositeShape):
+    inner_h: float=INNER_CYL_THICKNESS
+    inner_r: float=INNER_CYL_R
+        
+    outer_h: float=OUTER_CYL_THICKNESS
+    outer_r: float=OUTER_CYL_R
+    
+    tab_depth: float=TAB_DEPTH
+    edge_dist: float=ad.dtfield(self_default=lambda s: s.outer_r + s.tab_depth)
+    half_tab: float=TAB_WITDH / 2
+    
+    tab_sweep_radians: float=ad.dtfield(
+        self_default=lambda s: 2 * np.arcsin(((s.half_tab) / s.inner_r) / 2))
+    
+    tab_arc_sweep_radians: float=ad.dtfield(
+        self_default=lambda s: 2 * np.arcsin(((s.half_tab - 4.5 / 2) / s.inner_r) / 2))
+    
+    half_tab_sweep_rotation: ad.GMatrix=ad.dtfield(
+        self_default=lambda s: ad.rotZ(radians=s.tab_sweep_radians))
+    
+    tab_bevel_end_point: np.ndarray=ad.dtfield(
+        self_default=lambda s: to_2d(s.half_tab_sweep_rotation * ad.GVector([0, s.outer_r, 0])))
+    
+    tab_bevel_control_point: np.ndarray=ad.dtfield(
+        self_default=lambda s: to_2d(s.half_tab_sweep_rotation * ad.GVector([0, s.edge_dist, 0])))
+    
+    EPSILON = 0.001 
+    
+    path: ad.Path=ad.dtfield(
+        self_default=lambda s: (PathBuilder()
+          .move([0,0], name='origin')
+          .line([0, s.edge_dist], 'centre_to_edge')
+          .arc_centre_sweep([0,0], sweep_angle_radians=s.tab_arc_sweep_radians, 
+                            name='tab_arc')
+          .spline(
+              (s.tab_bevel_control_point, s.tab_bevel_end_point), 
+              cv_len=(2, 1),
+              name='tab_corner_curve')
+          .line([0, 0], 'edge_to_centre')
+          .build()))
+    
+    h: float=OUTER_CYL_THICKNESS
+    
+    extrude_node: Node=ad.ShapeNode(LinearExtrude, {'h': 'outer_h'}, 'path')
+    cyl_tab_cage_node: Node=ad.ShapeNode(ad.Cylinder, {'h': 'outer_h', 'r': 'edge_dist'})
+    cyl_tab_cage: Node=Node(ad.cageof, prefix='cyl_tab_cage_')
+    
+    fn: int=32
+    
+    KEY_ANCHOR=ad.surface_args(
+        ('tab', 0), 'half_tab_wedge', 'tab_arc', 0)
+    
+    EXAMPLE_ANCHORS=(ad.surface_args('key'),
+                     ad.surface_args(
+                         ('tab', 1), 'half_tab_wedge', 'tab_arc', 0))
+    EXAMPLE_SHAPE_ARGS=ad.args(cyl_tab_cage_hide_cage=False)
+    
+    def build(self):
+        shape = self.extrude_node()
+        
+        tab = shape.solid('half_tab_wedge')\
+            .at('centre_to_edge', 0, rh=0.5, pre=ad.tranX(self.EPSILON))
+        
+        tab.add_at(shape.solid('flipped_half_tab_wedge').
+                     at('centre_to_edge', 0, rh=0.5, 
+                        post=ad.ROTX_180, 
+                        pre=ad.tranX(-self.EPSILON)),
+                     'centre_to_edge', 0, rh=0.5)
+        
+        self.cyl_tab_cage_shape = self.cyl_tab_cage_node()
+        maker = self.cyl_tab_cage().at('centre')
+        
+        for i in range(3):
+            maker.add_at(tab.solid(('tab', i)).at('tab_arc', 0, rh=0.5), 
+                     'surface', degrees=i * 120, rh=0.5)
+        
+        return maker   
+    
+    @ad.anchor('key mount axis')
+    def key(self, tab=0):
+        return self.at(('tab', tab), 'half_tab_wedge', 'tab_arc', 0)
+    
+@ad.shape
+@datatree
+class CapAndLockAssembly(ad.CompositeShape):
+
+    tabs_node: Node=ad.ShapeNode(LockTabsAssembly)
+    inner_cyl_node: Node=ad.ShapeNode(ad.Cylinder, prefix='inner_')
+    outer_cyl_node: Node=ad.ShapeNode(ad.Cylinder, prefix='outer_')  
+
+    key_height: float=KEY_HEIGHT
+    key_thickness: float=KEY_THICKNESS
+    key_offset: float=KEY_OFFSET
+
+    key_size: tuple=ad.dtfield(
+        self_default=lambda s: (s.key_thickness, s.key_height, s.key_offset * 2))
+    key_node: Node=ad.ShapeNode(ad.Box, prefix='key_')
+    
+    cap_cage_shape: ad.Shape=None
+    cap_cage: Node=Node(ad.cageof, prefix='cap_cage_')
+    
+    thumbhole_r_base: float=7
+    thumbhole_r_top: float=8.5
+    thumbhole_h: float=INNER_CYL_THICKNESS + OUTER_CYL_THICKNESS
+    
+    thumbhole_node: Node=ad.ShapeNode(ad.Cone, prefix='thumbhole_')
+    
+    fn: int=128
+    
+    EXAMPLE_ANCHORS=()
+    EXAMPLE_SHAPE_ARGS=ad.args(cap_cage_hide_cage=True)
+    
+    
+    def build(self):
+        self.cap_cage_shape = ad.Cylinder(
+            h=self.outer_h + self.inner_h, r=self.outer_r)
+        
+        maker = self.cap_cage().at('centre', post=ad.ROTX_180)
+        
+        inner_cyl = self.inner_cyl_node()
+        
+        maker.add_at(inner_cyl.solid('inner_cyl').at('base'), 'base')
+        
+        outer_cyl = self.outer_cyl_node()
+        
+        maker.add_at(outer_cyl.solid('outer_cyl').at('top'), 'top')
+        
+        tabs = self.tabs_node()
+        
+        maker.add_at(tabs.solid('tabs').at('base'), 'outer_cyl', 'base')
+        
+        key = self.key_node()
+        
+        maker.add_at(key.solid('key').at('face_edge', 4, 2, 
+                                         post=ad.ROTZ_180 * ad.tranZ(self.key_offset)), 
+                                         'tabs', 'key')
+        
+        thumbhole = self.thumbhole_node()
+        
+        for i in range(3):
+            maker.add_at(thumbhole.hole(('thumb', i)).at('base'), 
+                         'tabs', 'key', i, post=ad.ROTX_270 * ad.tranY(17) * ad.tranZ(3))
+        
+        return maker    
+
+@ad.shape
+@datatree
+class SensorCap(ad.CompositeShape):
+    assembly_node: Node=ad.ShapeNode(CapAndLockAssembly)
+
+    EXAMPLE_SHAPE_ARGS=ad.args(fn=256, cyl_tab_cage_hide_cage=True)
+
+    def build(self):
+        return self.assembly_node().composite('assembly').at('base', rh=1)
+
+
+# Uncomment the line below to default to writing OpenSCAD files
+# when anchorscad_main is run with no --write or --no-write options.
+MAIN_DEFAULT=ad.ModuleDefault(all=True)
+   
+if __name__ == '__main__':
+    ad.anchorscad_main(False)
+
+    
+    
