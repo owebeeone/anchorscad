1d8782939fdfa57c4fc9917aac6a584dc6a8c37c
Gianni Mariani <owebeeone@gmail.com>
2025-01-15 00:17:38 +1100

    Removed anchorscad files now in anchorscad-core.

--- src/anchorscad/fab/config/printers/prusa/fdm.py
+++
@@ -1,8 +0,0 @@
-
-
-from anchorscad.fab.config.printers.printers import PrinterConfig
-
-
-Mini = PrinterConfig((180, 180, 180))
-Mk3 = PrinterConfig((250, 210, 210))
-Mk2 = PrinterConfig((250, 210, 210))
